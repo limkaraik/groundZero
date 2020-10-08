0d8710cc13e4c8f7e7c373203a0f24eb1426c286
Kar Aik Lim <limkaraik@hotmail.com>
2020-10-08 15:31:43 +0800

    minor details

--- the app/client/src/components/views/Meeting/Meeting.js
+++ the app/client/src/components/views/Meeting/Meeting.js
@@ -11,12 +11,38 @@ function Meeting() {
             </div>
             <br/>
             <div>
-                <Title level={4} style={{textAlign: 'center'}}>Agenda 1: Marketing Strategy</Title>
-                <Row>
+                <Row style={{textAlign:'center',  justifyContent:'center'}}>
                     <Text style={{color: '#1B7CED'}}>Kar Aik:</Text>
-                    <Text style={{marginLeft:'5px'}}>I am so cool</Text>
+                    <Text style={{marginLeft:'5px'}}>Once the call starts the audio will be fed into a cloud storage where a speech to  text model will process the streamed input.</Text>
                 </Row>
+                <Row style={{textAlign:'center',  justifyContent:'center'}}>
+                    <Text style={{color: '#1B7CED'}}>Kar Aik:</Text>
+                    <Text style={{marginLeft:'5px'}}>We can also use the chat function to text members in the call, and these chat will also be sored in the meeting</Text>
+                </Row>
+                <Row style={{textAlign:'center',  justifyContent:'center'}}>
+                    <Text style={{color: '#1B7CED'}}>Kar Aik:</Text>
+                    <Text style={{marginLeft:'5px'}}>Next the agenda can be set by anyone in the call which is important in the formatting of the meeting.</Text>
+                </Row>
+                <Row style={{textAlign:'center',  justifyContent:'center'}}>
+                    <Text style={{color: '#1B7CED'}}>Kar Aik:</Text>
+                    <Text style={{marginLeft:'5px'}}>So let us set agenda 1 to be marketing strategy</Text>
+                </Row>
+                <br/>
+                <Title level={4} style={{textAlign: 'center'}}>Agenda 1: Marketing Strategy</Title>
+                <Row style={{textAlign:'center',  justifyContent:'center'}}>
+                    <Text style={{color: '#1B7CED'}}>Kar Aik:</Text>
+                    <Text style={{marginLeft:'5px'}}>This will help all the meeting participants to be on the same page, hence reducing wasted time.</Text>
+                </Row>
+                <Row style={{textAlign:'center',  justifyContent:'center'}}>
+                    <Text style={{color: '#1B7CED'}}>Kar Aik:</Text>
+                    <Text style={{marginLeft:'5px'}}>Next, we an set agenda 2 to be sales target</Text>
+                </Row>
+                <br/>
                 <Title level={4} style={{textAlign: 'center'}}>Agenda 2: Sales Target</Title>
+                <Row style={{textAlign:'center',  justifyContent:'center'}}>
+                    <Text style={{color: '#1B7CED'}}>Kar Aik:</Text>
+                    <Text style={{marginLeft:'5px'}}>Then end the call.</Text>
+                </Row>
             </div>
         </div>
     )
